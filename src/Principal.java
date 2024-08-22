34b5574daf69d676570f4eeb05b6a50565463169
Filipe <filipe.cutri18@gmail.com>
2024-08-22 00:38:09 -0300

    Usando encapsulamento

--- src/Principal.java
+++ src/Principal.java
@@ -1,20 +1,19 @@
+import filmora.modelos.Filme;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setIncluidoNoPlano(true);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
-        System.out.println(meuFilme.retornarMedia());
-        System.out.println("Teste de commit");
+        System.out.println("Média das avaliações: " + meuFilme.retornarMedia());
     }
 }
