2a420e8d1b625dc27cac7d6aa6937b6a76a686ef
Filipe <filipe.cutri18@gmail.com>
2024-09-11 00:30:40 -0300

    Usando polimorfismo

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,4 @@
+import filmora.calculos.CalculadoraDeTempo;
 import filmora.modelos.Filme;
 import filmora.modelos.Serie;
 
@@ -28,5 +29,17 @@ public class Principal {
         serie.setMinutosPorEpisodios(50);
         serie.exibeFichaTecnica();
         System.out.println("Quantidade em minutos para maratonar Breaking Bad: " + serie.getDuracaoEmMinutos() + " minutos");
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Top Gun");
+        outroFilme.setAnoDeLancamento(2022);
+        outroFilme.setDuracaoEmMinutos(150);
+        outroFilme.setIncluidoNoPlano(true);
+
+        CalculadoraDeTempo calculadoraDeTempo = new CalculadoraDeTempo();
+        calculadoraDeTempo.inclui(meuFilme);
+        calculadoraDeTempo.inclui(outroFilme);
+        calculadoraDeTempo.inclui(serie);
+        System.out.println(calculadoraDeTempo.getTempoTotal());
     }
 }
---
+++ src/filmora/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,15 @@
+package filmora.calculos;
+
+import filmora.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal = 0;
+
+    public int getTempoTotal() {
+        return this.tempoTotal;
+    }
+
+    public void inclui(Titulo titulo){
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
